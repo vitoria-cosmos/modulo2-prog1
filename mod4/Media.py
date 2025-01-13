2b51de810d62887545773fe2caa7ea30f88d403b
vitoria.sousa <maria.v.cosmo@gmail.com>
2025-01-13 12:45:23 -0300

    aprendendo sobre estruturas condicionais

---
+++ mod4/Media.py
@@ -0,0 +1,11 @@
+notaA = float(input("Informe a primeira nota: "))
+notaB = float(input("Informe a segunda nota: "))
+
+# calcular media
+mediafinal = (notaA + notaB) / 2
+
+# verificação
+if mediafinal >= 7.0:
+    print("A média %.1f - Aprovado " % mediafinal)
+else:
+    print("A média: %.1f - Reprovado " % mediafinal)
